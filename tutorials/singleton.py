5bc4afdfb8f901e23cd6ca9014c7ae297a1657ab
JohnChiotis <johnchiotis@gmail.com>
2015-06-10 18:56:09 +0100

    Update singleton.py

--- tutorials/singleton.py
+++ tutorials/singleton.py
@@ -1,9 +1,9 @@
 class Singleton(type):
     instances = {}
-    def __call__(cls, *a, **k):
-        if cls.__name__ not in Singleton.instances:
-            Singleton.instances[cls.__name__] = type.__call__(cls, *a, **k)
-        return Singleton.instances[cls.__name__]
+    def __call__(self, *a, **k):
+        if self.__name__ not in Singleton.instances:
+            Singleton.instances[self.__name__] = type.__call__(self, *a, **k)
+        return Singleton.instances[self.__name__]
 
 
 class Instance(object):
@@ -14,8 +14,29 @@ class Instance(object):
         self.var = var
 
 
+class Singleton2(object):
+    instances = {}
+    def __new__(cls, *a, **k):
+        if cls.__name__ not in Singleton.instances:
+            Singleton.instances[cls.__name__] = object.__new__(cls, *a, **k)
+        return Singleton.instances[cls.__name__]
+
+
+class Instance2(Singleton2):
+    def __init__(self, var):
+        print "Init: setting var to: %s" % var
+        self.var = var
+
+
 if __name__ == '__main__':
+    print "First way"
     inst1 = Instance('first')
     inst2 = Instance('second')
     print 'Instances:', inst1, inst2
+    print 'Vars:', inst1.var, inst2.var
+    
+    print "\nSecond way"
+    inst1 = Instance2('first')
+    inst2 = Instance2('second')
+    print 'Instances same:', inst1 is inst2
     print 'Vars:', inst1.var, inst2.var
